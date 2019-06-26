b933f27734c75397c686c8c8a6c8d533fd982fa6
Simon <simon.beal@cambridge-intelligence.com>
2019-06-26 16:06:50 +0100

    Complete Slack setup

--- frontend/src/pages/slack_setup.js
+++ frontend/src/pages/slack_setup.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
-import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react';
+import { Button, Form, Grid, Header, Image, Segment, Label } from 'semantic-ui-react';
 
 import update from 'immutability-helper';
 import axios from 'axios';
 
 const authURL = "https://slack.com/api/auth.test";
-const infoURL = "https://slack.com/api/bots.info";
+const infoURL = "https://slack.com/api/users.info";
 
 class SlackSetup extends Component {
   constructor(props) {
@@ -22,19 +22,22 @@ class SlackSetup extends Component {
   
   slackLogin(callback) {
     axios.get(authURL, { params: { token: this.state.botToken }}).then(response => {
+      if (!response.data.ok) {
+        return callback("Invalid bot token");
+      }
       axios.get(infoURL, { params: { 
         token: this.state.botToken,
-        bot: response.data.user_id 
+        user: response.data.user_id 
       }}).then(response => {
         this.setState(update(this.state, {
           $merge: {
+            botUsername: response.data.user.name,
+            avatarUrl: response.data.user.profile.image_72
           }
         }), () => {
           callback("");
         });
       })
-    }).catch(response => {    
-      callback("Invalid bot token");
     });
   }
 
@@ -43,39 +46,82 @@ class SlackSetup extends Component {
       <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header as='h2' textAlign='center'>
-            <Segment stacked color='purple' inverted>
-              <Image src='/Slack_RGB_white.svg'/>
-              Connect to Slack
-            </Segment>
+            <Segment.Group stacked>
+              <Segment stacked color='purple' inverted>
+                <Image src='/Slack_RGB_white.svg'/>
+              </Segment>
+              { this.state.botUsername === "" && 
+                <Segment>
+                  Connect to Slack
+                </Segment>
+              }
+              { this.state.botUsername !== "" && 
+                <Segment>
+                  <Image src={this.state.avatarUrl} size='tiny' circular verticalAlign='middle' spaced='right'/>
+                  { this.state.botUsername }
+                </Segment>
+              }
+              { this.state.statusMessage &&               
+                <Segment>
+                  <Label color="red">
+                    { this.state.statusMessage }
+                  </Label>
+                </Segment>
+              }
+            </Segment.Group>
           </Header>
           <Form size='large'>
-            <Segment stacked>
-              <Form.Input fluid label="Bot OAuth2 Token" onChange={(event, data) => {
+            { this.state.botUsername === "" &&
+              <Segment stacked>
+                <Form.Input fluid label="Bot OAuth2 Token" type="password" onChange={(event, data) => {
+                    this.setState(update(this.state, {
+                        $merge: {botToken: data.value}
+                      }));
+                  }}
+                />
+                <Button color='purple' fluid size='large' onClick={() => {
+                  if (this.state.loading) {
+                    return
+                  }
                   this.setState(update(this.state, {
-                      $merge: {botToken: data.value}
-                    }));
-                }}
-              />
-              <Button color='purple' fluid size='large' onClick={() => {
-                if (this.state.loading) {
-                  return
-                }
-                this.setState(update(this.state, {
-                  $merge: {loading: true}
-                }), () => {
-                  this.slackLogin((status) => {
+                    $merge: {loading: true}
+                  }), () => {
+                    this.slackLogin((status) => {
+                      this.setState(update(this.state, {
+                        $merge: {
+                          loading: false,
+                          statusMessage: status
+                        }
+                      }));
+                    });
+                  });
+                }}>
+                  Check
+                </Button>
+              </Segment>
+            }
+            { this.state.botUsername !== "" && 
+              <Segment>
+                <Button color='purple' fluid size='large' attached='top' onClick={() => {
+                  }}
+                >
+                  Continue
+                </Button>
+                <Button color='purple' fluid size='large' attached='bottom' onClick={() => {
                     this.setState(update(this.state, {
                       $merge: {
-                        loading: false,
-                        statusMessage: status
+                        botToken: "",
+                        clientSecret: "",
+                        avatarUrl: "",
+                        botUsername: ""
                       }
                     }));
-                  });
-                });
-              }}>
-                Check
-              </Button>
-            </Segment>
+                  }}
+                >
+                  Use a different account
+                </Button>
+              </Segment>
+            }
           </Form>
         </Grid.Column>
       </Grid>
