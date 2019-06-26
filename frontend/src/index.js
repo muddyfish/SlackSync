fddbb0fcc5b987231dc016377ba541cb19bd8af4
Blue <redacted>
2019-06-26 20:23:18 +0100

    Rename App.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import Setup from './setup';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Setup/>, document.getElementById('root'));
