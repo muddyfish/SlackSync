dc3f757e3dab3c242d07cf8c9ce713d25a6ed678
Simon <simon.beal@cambridge-intelligence.com>
2019-06-26 15:48:01 +0100

    Discord setup now functional Slack setup untested

--- frontend/src/pages/discord_setup.js
+++ frontend/src/pages/discord_setup.js
@@ -1,30 +1,151 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Button, Form, Grid, Header, Image, Segment, Label } from 'semantic-ui-react'
 
-import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
+import update from 'immutability-helper';
+import axios from 'axios';
 
-function DiscordSetup() {
-  return (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header  as='h2' color='violet' textAlign='center'>
-          <Segment stacked color='grey' inverted>
-            <Image src='/Discord-Logo+Wordmark-Color.svg'/>
-            Connect to Discord
-          </Segment>
-        </Header>
-        <Form size='large'>
-          <Segment stacked  color='grey' inverted>
-            <Form.Input fluid label="Bot Token"/>
-            <Form.Input fluid label="Client Secret"/>
+const apiURL = "https://discordapp.com/api/v6/users/@me"
 
-            <Button color='violet' fluid size='large'>
-              Check
-            </Button>
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  );
+class DiscordSetup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      botToken: "",
+      clientSecret: "",
+      loading: false,
+      statusMessage: "",
+      avatarUrl: "",
+      botUsername: ""
+    }
+  }
+
+  discordLogin(callback) {
+    axios.get(apiURL, { headers: { Authorization: `Bot ${this.state.botToken}` }}).then(response => {
+      this.setState(update(this.state, {
+        $merge: {
+          botUsername: response.data.username,
+          avatarUrl: this.getAvatarURL(response.data)
+        }
+      }), () => {
+        callback("");
+      });
+    }).catch(response => {    
+      callback("Invalid bot token");
+    });
+  }
+
+  getAvatarURL(botData) {
+    if (botData.avatar) {
+      return `https://cdn.discordapp.com/avatars/${botData.id}/${botData.avatar}.png`
+    }
+    return `https://cdn.discordapp.com/embed/avatars/${parseInt(botData.discriminator) % 5}.png`
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' color='violet' textAlign='center'>
+            <Segment.Group stacked>
+              <Segment color='grey' inverted>
+                <Image src='/Discord-Logo+Wordmark-Color.svg'/>
+              </Segment>
+              { this.state.botUsername === "" && [
+                <Segment key="connect" color='grey' inverted>
+                  Connect to Discord
+                </Segment>,
+                <Segment key="portal" color='grey' inverted>
+                  <Button color='violet' onClick={() => {
+                    window.open("https://discordapp.com/developers/applications/", '_blank').focus();
+                  }}>
+                    Open Developer Portal
+                  </Button>
+                </Segment>
+              ]}
+              { this.state.botUsername !== "" && 
+                <Segment color='grey' inverted>
+                  <Image src={this.state.avatarUrl} size='tiny' circular verticalAlign='middle' spaced='right'/>
+                  { this.state.botUsername }
+                </Segment>
+              }
+              { this.state.statusMessage &&               
+                <Segment color='grey' inverted>
+                  <Label color="red">
+                    { this.state.statusMessage }
+                  </Label>
+                </Segment>
+              }
+            </Segment.Group>
+          </Header>
+          <Form size='large'>
+            { this.state.botUsername === "" && 
+              <Segment stacked color='grey' inverted>
+                <Form.Input fluid label="Bot Token" type="password" onChange={(event, data) => {
+                  this.setState(update(this.state, {
+                      $merge: {botToken: data.value}
+                    }));
+                }}/>
+                <Form.Input fluid label="Client Secret" type="password" onChange={(event, data) => {
+                  this.setState(update(this.state, {
+                      $merge: {clientSecret: data.value}
+                    }));
+                }}/>
+                <Button 
+                  color='violet'
+                  fluid 
+                  size='large' 
+                  onClick={() => {
+                    if (this.state.loading) {
+                      return
+                    }
+                    this.setState(update(this.state, {
+                      $merge: {loading: true}
+                    }), () => {
+                      this.discordLogin((status) => {
+                        this.setState(update(this.state, {
+                          $merge: {
+                            loading: false,
+                            statusMessage: status
+                          }
+                        }));
+                      });
+                    });
+                  }}
+                  className={this.state.loading? "loading": ""}
+                  disabled={this.state.botToken === "" || this.state.clientSecret === ""}
+                >
+                  Check
+                </Button>
+              </Segment>
+            }
+            { this.state.botUsername !== "" && 
+              <Segment>
+                <Button color='violet' fluid size='large' attached='top' onClick={() => {
+                  }}
+                >
+                  Continue
+                </Button>
+                <Button color='violet' fluid size='large' attached='bottom' onClick={() => {
+                    this.setState(update(this.state, {
+                      $merge: {
+                        botToken: "",
+                        clientSecret: "",
+                        avatarUrl: "",
+                        botUsername: ""
+                      }
+                    }));
+                  }}
+                >
+                  Use a different account
+                </Button>
+              </Segment>
+            }
+          </Form>
+        </Grid.Column>
+      </Grid>
+    )
+  }
 }
 
 export default DiscordSetup;
--- frontend/src/pages/slack_setup.js
+++ frontend/src/pages/slack_setup.js
@@ -1,29 +1,86 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react';
 
-function SlackSetup() {
-  return (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' textAlign='center'>
-          <Segment stacked color='purple' inverted>
-            <Image src='/Slack_RGB_white.svg'/>
-            Connect to Slack
-          </Segment>
-        </Header>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input fluid label="Bot OAuth2 Token"/>
+import update from 'immutability-helper';
+import axios from 'axios';
 
-            <Button color='purple' fluid size='large'>
-              Check
-            </Button>
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  );
+const authURL = "https://slack.com/api/auth.test";
+const infoURL = "https://slack.com/api/bots.info";
+
+class SlackSetup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      botToken: "",
+      loading: false,
+      statusMessage: "",
+      avatarUrl: "",
+      botUsername: ""
+    }
+  }
+  
+  slackLogin(callback) {
+    axios.get(authURL, { params: { token: this.state.botToken }}).then(response => {
+      axios.get(infoURL, { params: { 
+        token: this.state.botToken,
+        bot: response.data.user_id 
+      }}).then(response => {
+        this.setState(update(this.state, {
+          $merge: {
+          }
+        }), () => {
+          callback("");
+        });
+      })
+    }).catch(response => {    
+      callback("Invalid bot token");
+    });
+  }
+
+  render () {
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' textAlign='center'>
+            <Segment stacked color='purple' inverted>
+              <Image src='/Slack_RGB_white.svg'/>
+              Connect to Slack
+            </Segment>
+          </Header>
+          <Form size='large'>
+            <Segment stacked>
+              <Form.Input fluid label="Bot OAuth2 Token" onChange={(event, data) => {
+                  this.setState(update(this.state, {
+                      $merge: {botToken: data.value}
+                    }));
+                }}
+              />
+              <Button color='purple' fluid size='large' onClick={() => {
+                if (this.state.loading) {
+                  return
+                }
+                this.setState(update(this.state, {
+                  $merge: {loading: true}
+                }), () => {
+                  this.slackLogin((status) => {
+                    this.setState(update(this.state, {
+                      $merge: {
+                        loading: false,
+                        statusMessage: status
+                      }
+                    }));
+                  });
+                });
+              }}>
+                Check
+              </Button>
+            </Segment>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default SlackSetup;
