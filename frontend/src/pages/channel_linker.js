46acf922d4f5636b4b3700edf43f44d4942ec66a
Simon <simon.beal@cambridge-intelligence.com>
2019-06-27 18:05:33 +0100

    Use some state in websockets

--- frontend/src/pages/channel_linker.js
+++ frontend/src/pages/channel_linker.js
@@ -1,24 +1,52 @@
 import React from 'react';
 
 import Websocket from 'react-websocket';
+import update from "immutability-helper";
 import queryString from 'querystring';
 
 
 class ChannelLinker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      channels: [],
+      links: []
+    }
+  }
+
   getApiURL() {
     return queryString.parse(window.location.search.slice(1)).callback || `${window.location.host}`;
   }
 
   onMessage(message) {
-    console.log(JSON.parse(message));
+    const data = JSON.parse(message);
+    console.log(this.state)
+    if (data.type === "channel_update") {
+      this.setState(update(this.state, {
+        $merge: {
+          channels: data.channels
+        }
+      }));
+    }
+    else if (data.type === "channel_links_update") {
+      this.setState(update(this.state, {
+        $merge: {
+          links: data.channel_links
+        }
+      }));
+    }
+    else {
+      alert(`Unknown message type ${data.type}`);
+    }
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <Websocket 
           url={`ws://${this.getApiURL()}/ws`} 
-          onMessage={this.onMessage}
+          onMessage={() => this.onMessage}
           reconnect={true}
           debug={true}
           ref={Websocket => {
