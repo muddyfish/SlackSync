2c4f9b12b2ffd3349b9d3426ee398c5c6c089b10
Blue <redacted>
2019-06-28 23:11:10 +0100

    Implement setup URL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 import Setup from './setup';
 import ChannelLinker from './pages/channel_linker';
 
-ReactDOM.render(<ChannelLinker/>, document.getElementById('root'));
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ChannelLinker}/>
+          <Route exact path="/setup" component={Setup}/>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
